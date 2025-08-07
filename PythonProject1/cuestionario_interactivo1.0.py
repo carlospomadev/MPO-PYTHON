279363b4b38901589240e2f43774e24a43e36630
carlospomadev_ <c.poma.ramirez@gmail.com>
2025-08-08 00:23:51 +0200

    Avance 2

--- PythonProject1/cuestionario_interactivo1.0.py
+++ PythonProject1/cuestionario_interactivo1.0.py
@@ -2,23 +2,23 @@
 def nuevo_juego():
     respuestas = []
     respuestas_correctas = 0
-    preguntas_num = 0
 
-    for key in preguntas:
-        print("________________________")
-        print(key)
-        for i in opciones[preguntas_num]:
+
+    for pregunta in preguntas:
+        print("\n________________________")
+        print(pregunta["pregunta"])
+        for i in pregunta["opciones"]:
             print(i)
 
-        respuesta = input("Ingresa la opción (A, B, C, D: ").upper()
+        respuesta = input("Ingresa la opción (A, B, C, D): ").upper()
         respuestas.append(respuesta)
 
-        respuestas_correctas += (preguntas.get(key), respuesta)
-        preguntas_num += 1
-    score()respuestas_correctas,respuesta)
+        respuestas_correctas += revisar_respuesta(pregunta["respuesta_correcta"],respuesta)
+
+    score(respuestas_correctas, respuestas)
 #---------------------------------
 def revisar_respuesta(respuesta_correcta, respuesta):
-    if respuesta_correcta == respuesta:
+    if respuesta == respuesta_correcta:
         print("Respuesta Correcta :)")
         return 1
     else:
@@ -26,33 +26,57 @@ def revisar_respuesta(respuesta_correcta, respuesta):
         return 0
 #---------------------------------
 def score(respuestas_correctas,respuestas):
-    print("-------------------------")
+    print("\n-------------------------")
     print("RESULTADO")
     print("-------------------------")
-#---------------------------------
+    print(f"Respuestas Correctas: {respuestas_correctas}/{len(preguntas)}")
+
+    puntaje = int((respuestas_correctas / len(preguntas)) *100)
+    print("Puntaje: " + str(puntaje) + "%")
+
 def juega_nuevamente():
-    pass
+    respuesta = input("\nQuieres jugar de nuevo? (Si o No): ").upper()
+    return respuesta == "SI"
+
 #---------------------------------
-preguntas = {"¿Cuál es la capital de España?":"A",
-             "¿Cuántos días tiene una semana?":"C",
-             "¿Cuál es el resultado de 2 + 2?":"B",
-             "¿Cuál es el océano más grande del mundo?":"C",
-             "¿Cuál es el color del cielo en un día despejado?":"B",
-             "¿Cuántas patas tiene una araña?":"B",
-             "¿Cuál es el planeta más cercano al sol?":"C",
-             "¿Qué fruta es conocida por tener muchas semillas?":"D",
-             "¿Cuál es el idioma oficial de Brasil?":"B",
-             "¿Cuál es el animal más rápido del mundo?":"A"
-}
-opciones = [["A. Madrid","B. Barcelona","C. Valencia","D. Sevilla"],
-            ["A. 5","B. 6","C. 7","D. 8"],
-            ["A. 3","B. 4","C. 5","D. 6"],
-            ["A. Atlántico","B. Índico","C. Pacífico","D. Ártico"],
-            ["A. Verde","B. Azul","C. Rojo","D. Amarillo"],
-            ["A. 6","B. 8","C. 10","D. 4"],
-            ["A. Venus","B. Marte","C. Mercurio","D. Tierra"],
-            ["A. Manzana","B. Naranja","C. Fresa","D. Granada"],
-            ["A. Español","B. Portugués","C. Inglés","D. Francés"],
-            ["A. Guepardo","B. León","C. Tigre""D. Águila"]]
+preguntas = [
+    {"pregunta": "¿Cuál es la capital de España?",
+     "opciones":["A. Madrid","B. Barcelona","C. Valencia","D. Sevilla"],
+     "respuesta_correcta": "A"},
+    {"pregunta":"¿Cuántos días tiene una semana?",
+     "opciones":["A. 5","B. 6","C. 7","D. 8"],
+     "respuesta_correcta": "C"},
+    {"pregunta":"¿Cuál es el resultado de 2 + 2?",
+     "opciones":["A. 3","B. 4","C. 5","D. 6"],
+     "respuesta_correcta": "B"},
+    {"pregunta": "¿Cuál es el océano más grande del mundo?",
+     "opciones":["A. Atlántico","B. Índico","C. Pacífico","D. Ártico"],
+     "respuesta_correcta": "C"},
+    {"pregunta": "¿Cuál es el color del cielo en un día despejado?",
+     "opciones": ["A. Verde","B. Azul","C. Rojo","D. Amarillo"],
+     "respuesta_correcta": "B"},
+    {"pregunta": "¿Cuántas patas tiene una araña?",
+     "opciones": ["A. 6","B. 8","C. 10","D. 4"],
+     "respuesta_correcta": "B"},
+    {"pregunta": "¿Cuál es el planeta más cercano al sol?",
+     "opciones": ["A. Venus","B. Marte","C. Mercurio","D. Tierra"],
+     "respuesta_correcta": "C"},
+    {"pregunta": "¿Qué fruta es conocida por tener muchas semillas?",
+     "opciones": ["A. Manzana","B. Naranja","C. Fresa","D. Granada"],
+     "respuesta_correcta": "D"},
+    {"pregunta": "¿Cuál es el idioma oficial de Brasil?",
+     "opciones": ["A. Español","B. Portugués","C. Inglés","D. Francés"],
+     "respuesta_correcta": "B"},
+    {"pregunta": "¿Cuál es el animal más rápido del mundo?",
+     "opciones": ["A. Guepardo","B. León","C. Tigre","D. Águila"],
+     "respuesta_correcta": "A"
+     }
+]
+
+
 nuevo_juego()
+while juega_nuevamente():
+    nuevo_juego()
+
+print("Bye!!")
 
